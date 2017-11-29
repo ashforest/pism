448e187ee0c7a311b3a61d97afe842b3783b542b
Stas <netu.97@mail.ru>
2017-11-29 22:10:47 +0300

    add files

---
+++ src/Echo.java
@@ -0,0 +1,7 @@
+import util.ConsoleWriter;
+
+class Echo {
+    public static void main(String[] args) {
+        ConsoleWriter.writeToConsole(args[0]);
+    }
+}
---
+++ src/util/ConsoleWriter.java
@@ -0,0 +1,7 @@
+package util;
+
+public class ConsoleWriter {
+    public static void writeToConsole(String str) {
+        System.out.println(str);
+    }
+}
